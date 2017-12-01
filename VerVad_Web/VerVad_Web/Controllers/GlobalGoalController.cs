8c42d3a4ea590302acf67529c3e4956e0e8bbaeb
Raakost <jeanettesydboege@gmail.com>
2017-12-01 22:40:43 +0100

    init,home page done
    Web app Homepage with table displaying existing globagols and options for editing and deleting.

---
+++ VerVad_Web/VerVad_Web/Controllers/GlobalGoalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VerVad_Web.Controllers
+{
+    public class GlobalGoalController : Controller
+    {
+        // GET: GlobalGoal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
