35b7be55bdc21fc47144f6107f9ad03f46af7b9c
Fuflino <St@networks.dk>
2017-12-04 09:25:29 +0100

    Global goal index partially fixed!

--- VerVad_Web/VerVad_Web/Controllers/GlobalGoalController.cs
+++ VerVad_Web/VerVad_Web/Controllers/GlobalGoalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLogic.Entities;
 
 namespace VerVad_Web.Controllers
 {
@@ -10,6 +11,16 @@ namespace VerVad_Web.Controllers
     {
         // GET: GlobalGoal
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateGlobalGoal()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateGlobalGoal(GlobalGoal gg)
         {
             return View();
         }
