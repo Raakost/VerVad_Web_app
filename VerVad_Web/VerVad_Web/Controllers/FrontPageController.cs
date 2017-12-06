6c26f970f8388f1664ba1e7f980e0db2f30089c3
Raakost <jeanettesydboege@gmail.com>
2017-12-06 12:04:25 +0100

    frontpage update view and viewmodel

---
+++ VerVad_Web/VerVad_Web/Controllers/FrontPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VerVad_Web.ViewModels.FrontPageUpdate;
+
+namespace VerVad_Web.Controllers
+{
+    public class FrontPageController : Controller
+    {
+        [HttpGet]
+        public ActionResult Update(int id)
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(FrontPageUpdateViewModel vm)
+        {
+
+            return View(vm);
+        }
+    }
+}
---
+++ VerVad_Web/VerVad_Web/ViewModels/FrontpageUpdate/FrontPageUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ServiceGateways.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerVad_Web.ViewModels.FrontPageUpdate
+{
+    public class FrontPageUpdateViewModel
+    {
+        public FrontPage Frontpage { get; set; }
+        public List<Language> Languages { get; set; }
+    }
+}
