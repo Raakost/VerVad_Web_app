c3f9fc7e3385be6c3b22f21efac555dbf2b61d39
Raakost <jeanettesydboege@gmail.com>
2017-12-07 14:40:25 +0100

    childrenstext controller and view, no methods implemeted

---
+++ VerVad_Web/VerVad_Web/Controllers/ChildrensTextController.cs
@@ -0,0 +1,17 @@
+﻿using ServiceGateways.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VerVad_Web.Controllers
+{
+    public class ChildrensTextController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
